1b65b6d85313bd417c9c737da99e6c6ecb20f2ae
9diin <weartstudio@naver.com>
2025-07-02 00:19:10 +0900

    ✨ [chore] 새로운 스레드 UI 작업

--- app/(protected)/new.tsx
+++ app/(protected)/new.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { KeyboardAvoidingView, Platform, Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import { Image, KeyboardAvoidingView, Platform, Pressable, ScrollView, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function NewPostScreen() {
@@ -14,8 +14,25 @@ export default function NewPostScreen() {
             >
                 <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
                     <View className="py-4 flex-1">
-                        <Text className="text-white text-lg font-semibold">구디사는 개발자 9Diin</Text>
-                        <TextInput value={text} onChangeText={setText} placeholder="새로운 소식이 있나요?" placeholderTextColor="#a3a3a3" className="text-white" multiline numberOfLines={4} style={{ minHeight: 120, textAlignVertical: "top" }} />
+                        <View className="flex-row items-start gap-2">
+                            {/* 프로필 이미지 */}
+                            <View className="w-12 h-12">
+                                <Image source={{ uri: "https://github.com/9diin.png" }} className="w-12 h-12 rounded-full" />
+                            </View>
+                            <View>
+                                <Text className="text-white text-lg font-semibold">구디사는 개발자 9Diin</Text>
+                                <TextInput
+                                    value={text}
+                                    onChangeText={setText}
+                                    placeholder="새로운 소식이 있나요?"
+                                    placeholderTextColor="#a3a3a3"
+                                    className="text-white"
+                                    multiline
+                                    numberOfLines={4}
+                                    style={{ minHeight: 120, textAlignVertical: "top" }}
+                                />
+                            </View>
+                        </View>
                         <View className="flex-row items-center justify-between mt-auto pt-4">
                             <Text className="text-neutral-400">누구에게나 답글 및 인용 허용</Text>
                             <Pressable onPress={() => console.log("post: ", text)} className="bg-white py-3 px-5 rounded-full self-end">
